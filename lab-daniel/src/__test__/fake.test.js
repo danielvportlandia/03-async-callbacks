a6baef0223549d3a72e245b7c4a9d5c4684cd643
danielvportlandia <danielshelton90@gmail.com>
2018-04-19 09:44:10 -0700

    test

---
+++ lab-daniel/src/__test__/fake.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const what = 'what';
+console.log(what);
+
+describe('initial test to make sure Travis works', () => {
+  it('should show that Travis is operating properly', () => {
+    expect(true).toEqual(true);
+  });
+});
